daacd70372737f3eda7095dc9b86c73b494342d1
Mansi02mohite <162323603+Mansi02mohite@users.noreply.github.com>
2024-07-17 16:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = 'YOUR_API_KEY';
+const apiKey = 'b6cbd785d65d908df80f194920fce289';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const locationInput = document.getElementById('locationInput');
